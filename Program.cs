e5268e208cdda0540e14fb1434431b1ddb78249d
Sasha <F712@rambler.ru>
2022-09-28 22:54:05 +0300

    Урок 9. Часть 3

--- Program.cs
+++ Program.cs
@@ -44,17 +44,33 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 
-int m = 3;
-int n = 2;
-System.Console.WriteLine(A(n, m));
+int m = 2;
+int n = 3;
+System.Console.WriteLine(A(m, n));
 
-int A(int n, int m)
+int A(int m, int n)
 {
-    if (n == 0)
-        return m + 1;
-    else
-      if ((n != 0) && (m == 0))
-        return A(n - 1, 1);
-    else
-        return A(n - 1, A(n, m - 1));
+    if (m == 0)
+        return n + 1;
+
+    if ((m > 0) && (n == 0))
+        return A(m - 1, 1);
+
+    return A(m - 1, A(m, n - 1));
 }
+
+
+// int AkkermanFunc(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     if (m > 0 && n == 0)
+//     {
+//         return AkkermanFunc(m - 1, 1);
+//     }
+//     return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+// }
+
+// Console.WriteLine(AkkermanFunc(3, 2));
