5d5b1645a51b6beb19f6706108f20113e045783a
Sasha <F712@rambler.ru>
2022-09-28 22:54:37 +0300

    Урок 9. Часть 3

--- Program.cs
+++ Program.cs
@@ -60,17 +60,3 @@ int A(int m, int n)
 }
 
 
-// int AkkermanFunc(int m, int n)
-// {
-//     if (m == 0)
-//     {
-//         return n + 1;
-//     }
-//     if (m > 0 && n == 0)
-//     {
-//         return AkkermanFunc(m - 1, 1);
-//     }
-//     return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
-// }
-
-// Console.WriteLine(AkkermanFunc(3, 2));
